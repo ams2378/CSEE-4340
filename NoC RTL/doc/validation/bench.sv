135f57b3bef355420ae9b71ccebf00d38b651312
Ashwin Ramachandran <aramachandran89@gmail.com>
2012-11-11 14:01:17 -0500

    Initial validation skeletons added

---
+++ NoC RTL/doc/validation/bench.sv
@@ -0,0 +1,46 @@
+/**
+* @filename  		bench.sv 
+*
+* @brief     		The file includes the software model of the router and the random verification tb
+* @authors   		Ayushi Rajeev <ar3110@columbia.edu>
+			Ashwin Ramachandran <ar2648@columbia.edu>
+*	     		
+*  	 
+*/
+
+class router_transaction;
+
+	function new();
+	endfunction
+
+endclass
+
+class router_test;
+
+	function void golden_result();
+	endfunction
+
+endclass
+
+class router_check;
+
+	function bit check_results();
+	endfunction
+
+endclass
+
+class router_env;
+
+	function configure();
+	endfunction
+
+endclass
+
+program tb (ifc.bench ds);
+
+	task do_cycle;
+	endtask;
+
+	initial begin
+	end
+endprogram
