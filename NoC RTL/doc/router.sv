8b0abafafdedfbdf5ea6760120c4eedbe6a04512
Ashwin Ramachandran <aramachandran89@gmail.com>
2012-11-11 14:22:32 -0500

    Fixed signal names in top-level file

--- NoC RTL/doc/router.sv
+++ NoC RTL/doc/router.sv
@@ -1,7 +1,9 @@
 /*-----------------------------------------------------
-Design Name :router
+Design Name : router
 File Name   : router.sv
 Function    : Top level file 
+
+author	    : Dechhin Lama <ddl2126@columbia.edu>
 *///-----------------------------------------------------
 
 
@@ -15,11 +17,11 @@ module router (
 	input [15:0] west_i,
 	input [15:0] local_i,
 	
-	input n_iner_i,
-	input s_iner_i,
-	input e_iner_i,
-	input w_iner_i,
-	input l_iner_i,
+	input n_incr_i,
+	input s_incr_i,
+	input e_incr_i,
+	input w_incr_i,
+	input l_incr_i,
 
 	output [15:0] north_o,
 	output [15:0] south_o,
@@ -27,11 +29,11 @@ module router (
 	output [15:0] west_o,
 	output [15:0] local_o,
 
-	output n_iner_o,
-	output s_iner_o,
-	output e_iner_o,
-	output w_iner_o,
-	output l_iner_o
+	output n_incr_o,
+	output s_incr_o,
+	output e_incr_o,
+	output w_incr_o,
+	output l_incr_o
 	);
 
 /*
