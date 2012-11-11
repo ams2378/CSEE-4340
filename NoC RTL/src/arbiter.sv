7ef9bc27d0056b21e63d14fa4ec8a089a232f101
Adil Sadik <ams2378@columbia.edu>
2012-11-11 15:39:15 -0500

    new src files

---
+++ NoC RTL/src/arbiter.sv
@@ -0,0 +1,32 @@
+/**
+* filename  		arbiter.sv 
+* brief			Arbiter module for the router     		
+* authors   		Adil sadik <ams2378@columbia.edu>
+* data creation		11/11/12	
+* 
+* 
+*	 
+*/
+
+module arbiter (
+
+	input [2:0] req_port_addr1_i,
+	input [2:0] req_port_addr2_i,	
+	input [2:0] req_port_addr3_i,
+	input [2:0] req_port_addr4_i,
+	input [2:0] req_port_addr5_i,
+
+	output [2:0] req_port_addr1_o,
+	output [2:0] req_port_addr2_o,	
+	output [2:0] req_port_addr3_o,
+	output [2:0] req_port_addr4_o,
+	output [2:0] req_port_addr5_o
+
+)
+
+
+
+
+
+
+
--- NoC RTL/src/queue.sv
+++ NoC RTL/src/queue.sv
@@ -1,1 +1,12 @@
-//queue
+
+
+
+module queue (
+
+	input clk,
+	input rst_n,
+	input pop_req_i,
+	input [15:0] data_i,		
+
+	output [15:0] data_o
+)
