b60df778d93434df2b08629dd28cbe4e7d8e9e97
Dedor Lama <ddl2126@columbia.edu>
2012-11-11 15:46:56 -0500

    few updates

--- NoC RTL/src/router.sv
+++ NoC RTL/src/router.sv
@@ -11,17 +11,62 @@ module router (
 	ifc.dut d
 	);
 
+
 /*
  * instantiate the queue buffers
- 
-queue inputbuffers(
-	.north_i(d.north_i),
-	.south_i(d.south_i),
-	.east_i(d.east_i),
- 	.west_i(d.west_i),
-	.local_i(d.local_i)
+ * north queue */
+queue north_buffer(
+	.clk(clk),
+	.rst(rst),
+
+	.north_i(data_i)	
 	);
-*/	
+
+/*
+ * south queue */
+queue south_buffer(
+	.clk(clk),
+	.rst(rst),
+
+	.south_i(data_i)	
+	);
+
+/*
+ * east queue */
+queue east_buffer(
+	.clk(clk),
+	.rst(rst),
+
+	.east_i(data_i)	
+	);
+
+/*
+ * west queue */
+queue west_buffer(
+	.clk(clk),
+	.rst(rst),
+
+	.west_i(data_i)	
+	);
+
+/*
+ * local queue */
+queue local_buffer(
+	.clk(clk),
+	.rst(rst),
+
+	.local_i(data_i)	
+	);
+
+
+/*
+ * instantiate the counters
+ * north neighbor counter */
+counter north_counter(
+	n_incr_i(incr_i)
+)
+
+
 
 endmodule
 
