a4935aa241148b3c52745e521badd134e3dc6ddc
Ashwin Ramachandran <aramachandran89@gmail.com>
2012-11-13 15:10:09 -0500

    update

--- NoC RTL/src/address_gen.sv
+++ NoC RTL/src/address_gen.sv
@@ -1,9 +1,9 @@
 /**
 * @filename  		address_gen.sv 
 *
-* @brief     		Generates the direction to which data flits need to be sent comparing current router address to destination address.
+* @brief     		Generates the direction to which data flits need to be sent, comparing current router address to destination address.
 
-* @author   		ddl2126	<ddl2126@columbia.edu>
+* @author   		Dechhin Lama	<ddl2126@columbia.edu>
 *	     		
 *  	 
 */
