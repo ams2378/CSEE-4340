95278b475e670e5a7fb415ef35a5dd3e8b38b9fb
Ashwin Ramachandran <aramachandran89@gmail.com>
2012-11-15 15:31:09 -0500

    Added FF tb

--- NoC RTL/src/FF.sv
+++ NoC RTL/src/FF.sv
@@ -8,28 +8,23 @@ author	    : Dechhin Lama <ddl2126@columbia.edu>
 
 
 module FF #(parameter WIDTH = 1) (
-	input clk,
-	input rst,
-	input write_en_i,
-	input [WIDTH-1:0] write_data_i,
-	
-	output [WIDTH-1:0] read_data_o
-);
+	ifc_FF d
+	);
 
 reg [WIDTH-1:0] data;
 
 /*
  * the write block
  */
-always_ff @(posedge clk) begin
-	if (rst)
+always_ff @(posedge d.clk) begin
+	if (d.rst)
 		data <= '0;
-	else if (write_en_i)
-		data <= write_data_i;
+	else if (d.write_en_i)
+		data <= d.write_data_i;
 	else
 		data <= data;		// keep writing the data back
 end
 
-assign read_data_o = data;
+assign d.read_data_o = data;
 
 endmodule
