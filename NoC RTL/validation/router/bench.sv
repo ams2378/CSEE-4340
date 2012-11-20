4d7356f2e464fd4507e7b6ac7af1e1dcd8560552
Ashwin Ramachandran <aramachandran89@gmail.com>
2012-11-20 14:27:38 -0500

    Added initial bench code

--- NoC RTL/validation/router/bench.sv
+++ NoC RTL/validation/router/bench.sv
@@ -9,9 +9,47 @@
  */
 
 class router_transaction;
+   int reset_density;
 
-   function new();
+   int north_density;
+   int south_density;
+   int east_density;
+   int west_density;
+   int local_density;
+
+   function new(int rst_d,
+		int north_d,
+		int south_d,
+		int east_d,
+		int west_d,
+		int loc_d);
+
+	reset_density = rst_d;
+	north_density = north_d;
+	south_density = south_d;
+	east_density = east_d;
+	west_density = west_d;
+	local_density = loc_d;
    endfunction
+
+   /*
+    * randomly generated values to determine if we have
+    * a transaction on any of the 5 input ports
+    */
+   rand bit reset_req;
+   rand bit north_req;
+   rand bit south_req;
+   rand bit east_req;
+   rand bit west_req;
+   rand bit local_req;
+
+   /*
+    * constrain the values so that they are 0 or 1 based on
+    * the randomly generated value
+    */
+   constraint density_dist {
+	reset_req dist {0:/100-reset_density, 1:/reset_density};
+   }
 
 endclass
 
@@ -35,6 +73,14 @@ class router_env;
    int warmup_time = 10;
    bit verbose = 1;
 
+   int reset_density = 10;
+
+   int north_density = 20;
+   int south_density = 20;
+   int east_density = 20;
+   int west_density = 20;
+   int local_density = 20;
+
    function configure(string filename);
       int file, value, chars_returned;
       int seed = 3;
@@ -52,6 +98,25 @@ class router_env;
 	 else if("VERBOSE" == param) begin
 	    verbose = value;
 	 end
+	 else if("RESET_DENSITY" == param) begin
+	    reset_density = value;
+	 end
+	 else if("NORTH_DENSITY" == param) begin
+	    north_density = value;
+	 end
+	 else if("SOUTH_DENSITY" == param) begin
+	    south_density = value;
+	 end
+	 else if("EAST_DENSITY" == param) begin
+	    east_density = value;
+	 end
+	 else if("WEST_DENSITY" == param) begin
+	    west_density = value;
+	 end
+	 else if("LOCAL_DENSITY" == param) begin
+	    local_density = value;
+	 end
+	 else if
 	 else begin
 	    $display ("Never heard of a: %s\n", param);
 	    $exit();
