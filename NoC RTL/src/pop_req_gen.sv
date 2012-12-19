3b98e0b038330653c594bdf8e22331b0a97d7d89
Dedor Lama <ddl2126@columbia.edu>
2012-12-19 16:45:37 -0500

    fixed xbar

--- NoC RTL/src/pop_req_gen.sv
+++ NoC RTL/src/pop_req_gen.sv
@@ -1,15 +1,33 @@
 module pop_req_gen (
-	input [2:0] address_route_i,
+	input [2:0] address_route_n,
+	input [2:0] address_route_s,
+	input [2:0] address_route_e,
+	input [2:0] address_route_w,
+	input [2:0] address_route_l,
+	
 	input [2:0] q_address,
 
-	input valid_i,
+	input valid_n,
+	input valid_s,
+	input valid_e,
+	input valid_w,
+	input valid_l,
+
 	output pop_req_o	
 );
 
-logic pop_req;
+logic pop_req_n;
 
 always_comb begin
-	if (address_route_i == q_address && valid_i)
+	if (address_route_n == q_address && valid_n)
+		pop_req = 1;
+	else if (address_route_s == q_address && valid_s)
+		pop_req = 1;
+	else if (address_route_e == q_address && valid_e)
+		pop_req = 1;
+	else if (address_route_w == q_address && valid_w)
+		pop_req = 1;
+	else if (address_route_l == q_address && valid_l)
 		pop_req = 1;
 	else
 		pop_req = 0;
